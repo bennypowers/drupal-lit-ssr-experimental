2e4554fa716205085c0d38ec8d6ed1aca5393d53
Benny Powers <web@bennypowers.com>
2024-05-16 12:56:19 +0300

    fix: event processor

---
+++ web/modules/custom/lit_ssr/src/EventSubscriber/SsrPostProcessSubscriber.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\patternkit\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * An event subscriber to post-process page markup through SSR.
+ */
+final class SsrPostProcessSubscriber implements EventSubscriberInterface {
+
+  protected static $ssr_url = 'http://drupal_lit_ssr_ssr:3333';
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents(): array {
+    return [
+      KernelEvents::RESPONSE => ['onKernelResponse'],
+    ];
+  }
+
+  /**
+   * Kernel response event handler.
+   */
+  public function onKernelResponse(ResponseEvent $event): void {
+    $response = $event->getResponse();
+    $url = $event->getRequest()->getUri();
+    if ($this->is_valid_ssr_url($url) && $content = $response->getContent()) {
+      // Process through SSR.
+      $processed = $this->processSsr($content, $url);
+
+      // Update the processed data back into the content.
+      $response->setContent($processed);
+    }
+  }
+
+  protected function is_valid_ssr_url(string $url): bool {
+    $path = parse_url($url, PHP_URL_PATH);
+    return (is_string($path)
+            && !str_starts_with('/admin/')
+            && !str_starts_with('/block/')
+            && !str_ends_with('.js')
+            && !str_ends_with('.css')
+           );
+  }
+
+  /**
+   * Submit HTML for processing through the SSR service.
+   *
+   * @param string $html
+   *   The HTML string to be processed through the SSR service.
+   *
+   * @return string
+   *   The processed HTML or the original markup if unsuccessful.
+   */
+  protected function processSsr(string $html, string $url): string {
+    $client = \Drupal::httpClient();
+    $response = $client->request('POST', self::$ssr_url, [
+      'body' => $html,
+      'headers' => [
+        'X-Drupal-Url' => $url,
+        'Content-Type' => 'text/html',
+        'Accept'     => 'text/html',
+      ],
+    ]);
+
+    if ($response->getStatusCode() != 200) {
+      return $html;
+    }
+    else {
+      return $response->getBody()->getContents();
+    }
+  }
+
+}
